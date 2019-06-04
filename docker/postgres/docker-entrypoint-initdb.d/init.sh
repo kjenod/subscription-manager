b834da60f46ef71b41543c88369ce335f782338e
Alex Ntavelos <a.ntavelos@gmail.com>
2019-06-04 12:11:10 +0200

    add ping-credentials endpoint

--- docker/postgres/docker-entrypoint-initdb.d/init.sh
+++ docker/postgres/docker-entrypoint-initdb.d/init.sh
@@ -7,4 +7,5 @@ sudo -u postgres bash << EOF
 createuser $DB_USER
 createdb subscriptions
 createdb testing
+insert into users(username, password, created_at, active, is_admin) values('admin', 'pbkdf2:sha256:150000$ljwbHben$e86cd91626df47864120f4d9220b27dab4de7883d646a9e26dc89e0eb71be4cd', CURRENT_TIMESTAMP, 't', 't');
 EOF
